a6c286177b79a24e3dd4cf75a288ef1feb37039b
lewkyb <luke92brown@gmail.com>
2021-03-25 17:23:19 -0500

    working login stuff? needs a lot of cleaning up

--- irctube/views.py
+++ irctube/views.py
@@ -1,22 +1,31 @@
-from flask import render_template, request
+from flask import Blueprint, render_template, request
 from werkzeug.utils import secure_filename
-import json
+
+from . import db
+
 import os
 
 from irctube import app
 
-@app.route('/')
+views = Blueprint('views', __name__)
+
+@views.route('/')
 def index():
     return render_template('home.html')
+
+@views.route('/profile')
+def profile():
+    return render_template('profile.html')
 
 @app.route('/upload_success', methods = ['GET', 'POST'])
 def get_file():
 
     if request.method == 'POST':
-        f = request.files['file']
-        f.save(secure_filename(f.filename))
+        file_from_user = request.files['user_image_file']
+        filename = secure_filename(file_from_user.filename)
+        file_from_user.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         
-        text = request.form['text']
+        text = request.form['message_to_encrypt_textbox']
         processed_text = text.upper()
 
-        return render_template('upload_success.html')
+        return render_template('upload_success.html', filename=filename)
